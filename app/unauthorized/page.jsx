af335534f1d4fd91f62e0c55ee5ff990ee1e3fc3
Masum Billah <masumbillah546@gmail.com>
2024-11-29 11:58:32 +0600

    fixing

--- app/unauthorized/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function UnauthorizedPage() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Unauthorized</h1>
-      <p>You do not have permission to access this page.</p>
-      <a href="/" className="btn btn-primary">Go Home</a>
-    </div>
-  );
-}
